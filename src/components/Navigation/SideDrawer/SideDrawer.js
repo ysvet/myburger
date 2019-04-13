3d4428db12e01391907678af77fcf9440162a527
ysvet <iurie.shvets@gmail.com>
2019-04-13 19:46:32 +0300

    changed folder structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import styles from './SideDrawer.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../Logo/Logo';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 
 const sideDrawer = ( props ) => {
     let attachedClasses = [styles.SideDrawer, styles.Close];
