91d7c798213a433a31199bdeb8beb222af8acc8e
ysvet <iurie.shvets@gmail.com>
2019-05-01 09:56:43 +0300

    added checkout container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 
@@ -9,7 +10,8 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder /> 
+          <BurgerBuilder />
+          <Checkout /> 
         </Layout>
       </div>
     );
