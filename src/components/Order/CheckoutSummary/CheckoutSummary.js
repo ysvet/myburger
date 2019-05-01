1c9a8d45d53d9a0dc66e097df5f5682de82b0936
ysvet <iurie.shvets@gmail.com>
2019-05-01 12:31:47 +0300

    navigating back and nex page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,15 +8,15 @@ const checkoutSummary = (props) => {
     return (
         <div className={styles.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
-            <div style={{width: '300px',  margin: 'auto'}}>
+            <div style={{width: '100%',  margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
             <Button 
                 btnType="Danger"
-                clicked>CANCEL</Button>
+                clicked={props.checkoutCancelled}>CANCEL</Button>
             <Button 
                 btnType="Success"
-                clicked>CONTINUE</Button>
+                clicked={props.checkoutContinued}>CONTINUE</Button>
         </div>
 
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,29 @@ class BurgerBuilder extends Component {
 
   puchaseContinueHandler = () => {
     // alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Yuri S',
-        address: {
-        street: 'Alexandrovskaia 100',
-        zipCode: '12998',
-        country: 'Bessarabia'
-        },
-        email: 'sajd@njkjdf.oisa'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post( '/orders.json', order )
-      .then( response =>  { 
-          this.setState({ loading: false, purchasing: false });
-      } )
-      .catch( error =>  { 
-        this.setState({ loading: false, purchasing: false  });
-    } );
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Yuri S',
+    //     address: {
+    //     street: 'Alexandrovskaia 100',
+    //     zipCode: '12998',
+    //     country: 'Bessarabia'
+    //     },
+    //     email: 'sajd@njkjdf.oisa'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post( '/orders.json', order )
+    //   .then( response =>  { 
+    //       this.setState({ loading: false, purchasing: false });
+    //   } )
+    //   .catch( error =>  { 
+    //     this.setState({ loading: false, purchasing: false  });
+    // } );
+    this.props.history.push('/checkout');
   }
 
   render() {
