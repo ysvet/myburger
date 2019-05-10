b298e30f0fa665b7d5f576c4ef284b717f9a1a3e
ysvet <iurie.shvets@gmail.com>
2019-05-10 09:18:42 +0300

    handling user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,19 +10,19 @@ const input = (props) => {
             inputElement = <input 
                 className={styles.InputElement} 
                 {...props.elementConfig} 
-                value={props.value} />;
+                value={props.value} onChange={props.changed} />;
             break;
         case ( 'textarea' ): 
             inputElement = <textarea 
                 className={styles.InputElement} 
                 {...props.elementConfig} 
-                value={props.value} />;
+                value={props.value} onChange={props.changed} />;
             break;
         case ( 'select' ): 
             inputElement = (
                 <select 
                     className={styles.InputElement} 
-                    value={props.value}>
+                    value={props.value} onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                         {option.displayValue}
@@ -35,7 +35,7 @@ const input = (props) => {
             inputElement = <input 
             className={styles.InputElement} 
             {...props.elementConfig} 
-            value={props.value} />;                                                                                     
+            value={props.value} onChange={props.changed} />;                                                                                     
     }
 
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -80,6 +80,19 @@ class ContactData extends Component {
         } );
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        // state is an object with nested objec so we need to deep clone it
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -96,7 +109,8 @@ class ContactData extends Component {
                             key={formElement.id}
                             elementType={formElement.config.elementType}
                             elementConfig={formElement.config.elementConfig}
-                            value={formElement.config.value} />
+                            value={formElement.config.value} 
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                     ))}
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
