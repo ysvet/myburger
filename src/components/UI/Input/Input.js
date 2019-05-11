6bcde02773e20fbf2521e4ed69e1697e48ecd93f
ysvet <iurie.shvets@gmail.com>
2019-05-11 12:14:03 +0300

    adding validation feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,24 +4,29 @@ import styles from './Input.module.css';
 const input = (props) => {
 
     let inputElement = null;
+    const inputStyles = [styles.inputElement];
+
+    if (props.invalid&&props.shouldValidate) {
+        inputStyles.push(styles.Invalid);
+    }
 
     switch ( props.elementType ) {
         case ( 'input' ):
             inputElement = <input 
-                className={styles.InputElement} 
+                className={inputStyles.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value} onChange={props.changed} />;
             break;
         case ( 'textarea' ): 
             inputElement = <textarea 
-                className={styles.InputElement} 
+                className={inputStyles.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value} onChange={props.changed} />;
             break;
         case ( 'select' ): 
             inputElement = (
                 <select 
-                    className={styles.InputElement} 
+                    className={inputStyles.join(' ')} 
                     value={props.value} onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
@@ -33,7 +38,7 @@ const input = (props) => {
             break;        
         default:
             inputElement = <input 
-            className={styles.InputElement} 
+            className={inputStyles.join(' ')} 
             {...props.elementConfig} 
             value={props.value} onChange={props.changed} />;                                                                                     
     }
